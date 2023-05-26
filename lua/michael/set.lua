a23fd55a0a3209b74c3bb6c8e8307252b6f0b092
Michael Ohayon <1michaelohayon@gmail.com>
2023-05-26 07:10:42 +0300

    t

--- lua/michael/set.lua
+++ lua/michael/set.lua
@@ -1,6 +1,6 @@
 
 
-vim.opt.nu = true
+--vim.opt.nu = true
 -- vim.opt.rnu = true
 
 
@@ -11,6 +11,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
+vim.opt.cursorline = true
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
@@ -23,7 +24,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "no"
+vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 -- vim.opt.colorcolumn = "80"
