61b6de928f2f23222485471aeaf255031dbf7d7b
Michael Ohayon <1michaelohayon@gmail.com>
2023-05-28 20:43:26 +0300

    27/5

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -40,7 +40,23 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  
+  vim.keymap.set("n", "<F3>", function() vim.lsp.buf.format() end, opts)
 end)
+
+
+
+lsp.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['lua_ls'] = {'lua'},
+    ['gopls'] = {'go'},
+    ['rust_analyzer'] = {'rust'},
+  }
+})
 
 lsp.setup()
 
